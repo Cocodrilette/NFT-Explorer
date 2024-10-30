0e41bdbd6d8561297bafb5e2e49ebded7cd18e9d
cocodrilette <cocodrilette@gmail.com>
2024-10-30 08:09:42 -0500

    first commit

---
+++ src/components/nftcard.jsx
@@ -0,0 +1,26 @@
+const NftCard = ({ image, id, title, address }) => {
+  return (
+    <div className="w-1/4 mr-3 mb-4 bg-slate-100 rounded-md">
+      <img className="w-full rounded-t-md" key={id} src={image}></img>
+      <div className="p-3">
+        <div className="flex mb-3">
+          <div className="flex-grow">
+            <h3 className="text-xl">{title}</h3>
+            <p>{id}</p>
+          </div>
+          <div className="flex mr-3">
+            <a
+              target="_blank"
+              className="text-blue-700"
+              href={`https://etherscan.io/token/${address}`}
+            >{`${address.slice(0, 4)}...${address.slice(
+              address.length - 4
+            )}`}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NftCard;
